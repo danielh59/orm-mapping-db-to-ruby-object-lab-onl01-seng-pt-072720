d1fe3a8425c7085ed926fbb047571f6b374bb74c
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-07 20:19:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -109,7 +109,16 @@ class Student
   end
 
   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+      SQL
 
+    DB[:conn].execute(sql, x).map do |row|
+        self.new_from_db(row)
+      end
   end
 
 end
