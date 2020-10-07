13eb300d71db2374b3535bb17c63c490c743aa8b
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-07 19:28:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,7 +105,10 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    self.first_X_students_in_grade_10(1)
+    x = self.first_X_students_in_grade_10(1)
+     x.map do |i|
+       self.new_from_db(i)
+     end[0]
   end
 
 end
