839b0a973058c2f1645ad7eca67faceb72a2324b
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-07 19:27:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,17 +105,7 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = 10
-      ORDER BY id
-      LIMIT ?
-      SQL
-
-     DB[:conn].execute(sql).map do |row|
-        self.new_from_db(row)
-      end
+    self.first_X_students_in_grade_10(1)
   end
 
 end
