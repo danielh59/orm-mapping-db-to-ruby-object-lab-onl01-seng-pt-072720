437b78931f6ec967ce85526130b1f09dfb536fad
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-07 19:23:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,9 +86,8 @@ class Student
       LIMIT ?
       SQL
 
-    x =  DB[:conn].execute(sql).map do |row|
+     DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
-          x[0]
       end
 
   end
